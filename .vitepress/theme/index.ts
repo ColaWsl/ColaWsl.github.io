c3effc9780c97235e8cae70780c8afa97700dfc4
Austin <wsl612425@163.com>
2024-07-01 20:21:25 +0800

    添加 medium-zoom 插件

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,14 +2,39 @@
 import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
-// import './style.css'
-import Layout from "./Layout.vue";
+import './style.css' // 控制主题美化
+// @ts-ignore
+import Layout from './Layout.vue';
 
+/* 添加 medium-zoom 插件
+ */
+import mediumZoom from 'medium-zoom';
+import { onMounted, watch, nextTick } from 'vue';
+import { useRoute } from 'vitepress';
+/**/
 
 import "vitepress-markdown-timeline/dist/theme/index.css";
 
+
 export default {
   extends: DefaultTheme,
+  /* medium-zoom 插件
+  */
+  setup() {
+    const route = useRoute();
+    const initZoom = () => {
+      // mediumZoom('[data-zoomable]', { background: 'var(--vp-c-bg)' }); // 默认
+      mediumZoom('.main img', { background: 'var(--vp-c-bg)' }); // 不显式添加{data-zoomable}的情况下为所有图像启用此功能
+    };
+    onMounted(() => {
+      initZoom();
+    });
+    watch(
+        () => route.path,
+        () => nextTick(() => initZoom())
+    );
+  },
+  /**/
   Layout: () => {
     // return h(DefaultTheme.Layout, null, {
     //   // https://vitepress.dev/guide/extending-default-theme#layout-slots
